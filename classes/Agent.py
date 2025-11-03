017a624d25f4bf27aef974a2ebf1c979e22ee9cf
DavidPopovich3261 <8534397@gmail.com>
2025-11-03 12:17:14 +0200

    Exercise 3

--- classes/Agent.py
+++ classes/Agent.py
@@ -3,7 +3,18 @@
 class Agent:
     def __init__(self,code_name,clearance_level):
         self.code_name=code_name
-        self.clearance_level=clearance_level
+        self._clearance_level=clearance_level
+
+
+    def setter(self,clearance_level):
+        if clearance_level>0 and clearance_level<11:
+            self._clearance_level=clearance_level
+
+
+    def getter(self):
+        print(f"{self._clearance_level}")
+
+
 
     def report(self):
         print(f"Agent {self.code_name} reporting. Clearance Level: {self.clearance_level}")
