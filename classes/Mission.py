d13ea5872595005c147cf8f21bdae20b44c08a13
DavidPopovich3261 <8534397@gmail.com>
2025-11-03 12:11:52 +0200

    Mission

--- classes/Mission.py
+++ classes/Mission.py
@@ -1,1 +1,15 @@
 
+
+
+class Mission:
+    def __init__(self,mission_name ,target_location ):
+        self.mission_name=mission_name
+        self.target_location=target_location
+        self.assigned_agent=None
+
+
+    def assign(self,assigned_agent):
+        self.assigned_agent=assigned_agent
+
+    def brief(self):
+        print(f"Mission: {self.mission_name}, Target: {self.target_location}, Agent: {self.assigned_agent.code_name}")
